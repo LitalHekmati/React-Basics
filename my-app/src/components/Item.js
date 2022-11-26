a287d626eb10a6fde179003e34ddc8936c063767
eden shviro <edenshviro82@gmail.com>
2022-11-26 16:27:47 +0200

    linking

--- my-app/src/components/Item.js
+++ my-app/src/components/Item.js
@@ -1,19 +1,28 @@
 
+import React, { useState } from 'react';
+import App from '../App';
+import Info from './Info';
+//import { useHistory } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+
 
 function Item(props){
-    return(
+    
+
+  
+  return(
+    <Link to={`/${props.title}`}>    
     <div className="card">
       <div className="top">
         <h2 className="name">{props.title}</h2>
         <img className="circle-img" src={props.img} alt="avatar_img" />
       </div>
       <div className="bottom">
-        <p className="info">{props.price}</p>
-        <button>View more info</button>
-        
+        <p className="info">{props.price}</p>     
       </div>
     </div>
-        
+    </Link>
     );
 
 
